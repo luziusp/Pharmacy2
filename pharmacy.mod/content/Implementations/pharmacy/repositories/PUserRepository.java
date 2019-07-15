602987c305fff70560acb7b6002d377f2b6c9497
Luzius Prantl <>
2019-07-15 21:02:12 +0200

    Revert "no message"
    This reverts commit 87c7a51c4db2056917327e3aede9fb3409a5eb90.

--- pharmacy.mod/content/Implementations/pharmacy/repositories/PUserRepository.java
+++ pharmacy.mod/content/Implementations/pharmacy/repositories/PUserRepository.java
@@ -3,6 +3,4 @@ package pharmacy.repositories;
 import pharmacy.types.PUserId;
 
 public class PUserRepository<ID extends PUserId, T extends pharmacy.types.PUser> extends pharmacy.repositories.base.PUserRepository<ID, T> {
-
-
 }
